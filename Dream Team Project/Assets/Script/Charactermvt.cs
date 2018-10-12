2d639de5a2bfc8089a00e766d120cfc07b9841fb
Ariel Nam <sagwa365nam@aol.com>
2018-10-12 16:43:05 -0700

    first push

---
+++ Dream Team Project/Assets/Script/Charactermvt.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactermvt : MonoBehaviour
+{
+	//making variables 
+	public Rigidbody2D myRigidbody;
+	public float speed = 5;
+	public float jumpForce = 8;
+
+	bool isGround = true;
+	private float horizontalInput;
+	
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D variable)
+	{
+		Debug.Log("gothere");
+		if (variable.gameObject.tag == "ground")
+			isGround = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get user input
+		horizontalInput = Input.GetAxis("Horizontal");
+		transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+		if (Input.GetButtonDown("Jump") && isGround == true)
+		{
+			Debug.Log("inair");
+			myRigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+			isGround = false;
+		}
+
+	}
+}
