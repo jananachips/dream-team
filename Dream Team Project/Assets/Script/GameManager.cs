06b0c89b948d4854b7a4a692ae40e4a740f18c88
deskedge <siweisijiao.weebly@gmail.com>
2018-11-08 21:47:28 -0800

    biao_commit test

---
+++ Dream Team Project/Assets/Script/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public GameObject pauseScreen;
+    private bool gamePaused = false;
+
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }else if (!gamePaused && Input.GetKeyDown(KeyCode.P))
+        {
+            RestartGame();
+        }
+    }
+
+    //to use this in other code:  FindObjectOfType<GameManager>().RestartGame()
+    public void RestartGame()
+    {
+        //build index: when we build the game, from top to bottom, start from 0
+        Debug.Log("Restarting ");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+    }
+
+    public void PauseGame()
+    {
+        //freeze time
+        Time.timeScale = 0f;
+        gamePaused = true;
+        pauseScreen.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        gamePaused = false;
+        pauseScreen.SetActive(false);
+    }
+
+    public bool IsPause()
+    {
+        return gamePaused;
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        //maybe we want do something here
+    }
+
+
+
+}
---
+++ Dream Team Project/Assets/Script/_test_playerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _test_playerMovement : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float speed;
+
+
+	void Start () {
+		
+	}
+    	
+    //from the another code file
+	void Update () {
+        float moveInput = Input.GetAxis("Horizontal");
+        //transform.position = transform.position + new Vector3(speed * moveInput * Time.deltaTime, 0, 0);
+        rb.position = rb.position + new Vector2(speed * moveInput * Time.deltaTime, 0);
+
+        
+
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("on ground");
+        }
+    }
+}
