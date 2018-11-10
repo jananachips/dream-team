580112d7e151354889da48a443e256205b0d4489
Ariel Nam <sagwa365nam@aol.com>
2018-11-09 17:08:47 -0800

    Includes: Start Menu 	-start game 	-quit game Pause Menu 	-pause game (press esc to pause and unpause)

---
+++ Dream Team Project/Assets/Script/Ariel/PauseManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+	public GameObject pauseUI;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			PauseHandler();
+		}
+	}
+
+	public void PauseHandler()
+	{
+		if (Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			pauseUI.SetActive(true);
+		}
+		else if (Time.timeScale == 0)
+		{
+			Time.timeScale = 1;
+			pauseUI.SetActive(false);
+		}
+	}
+}
---
+++ Dream Team Project/Assets/Script/Ariel/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIManager : MonoBehaviour {
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+}
